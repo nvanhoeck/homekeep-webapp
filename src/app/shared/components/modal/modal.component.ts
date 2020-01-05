cdec6882580614044a7451a16df85fa6528c157b
Niko VH <niko.vanhoeck@gmail.com>
2020-01-05 12:25:17 +0100

    [HOME]: Prepare content injection for modal

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,28 +1,89 @@
-import {Component, OnInit} from '@angular/core';
+import {
+  AfterContentInit,
+  ChangeDetectorRef,
+  Compiler,
+  Component,
+  ComponentFactoryResolver,
+  ComponentRef,
+  OnChanges,
+  OnDestroy,
+  Type,
+  ViewChild,
+  ViewContainerRef
+} from '@angular/core';
 import {ButtonClass, ButtonSize, ButtonType} from '../buttons';
 import {ModalService} from './services/modal.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnChanges, AfterContentInit, OnDestroy {
+  @ViewChild('injected', {read: ViewContainerRef, static: false}) target;
+
+  public injectedType: Type<any> | any;
+  public injectedProperties: ComponentPropertyModel[];
+
   buttonType: ButtonType = ButtonType.PRIMARY;
   buttonSize: ButtonSize = ButtonSize.MEDIUM;
   buttonClass: ButtonClass = ButtonClass.ICON;
 
+  compRef: ComponentRef<any>;
+  private isViewInitialized = false;
+
   public listener: ModalService;
 
-  constructor() {
+  constructor(private readonly compiler: Compiler,
+              private readonly cdRef: ChangeDetectorRef,
+              private readonly componentFactoryResolver: ComponentFactoryResolver) {
   }
 
-  ngOnInit() {
+  updateComponent() {
+    if (!this.isViewInitialized) {
+      return;
+    }
+    if (this.compRef) {
+      this.compRef.destroy();
+    }
+
+    const factory = this.componentFactoryResolver.resolveComponentFactory(this.injectedType);
+    this.compRef = this.target.createComponent(factory);
+
+    if (this.injectedProperties) {
+      for (const property of this.injectedProperties) {
+        _.set(this.compRef.instance, property.key, property.value);
+      }
+    }
+
+    this.cdRef.detectChanges();
   }
+
+  ngOnChanges() {
+    this.updateComponent();
+  }
+
+  ngAfterContentInit() {
+    this.isViewInitialized = true;
+    this.updateComponent();
+  }
+
+  ngOnDestroy() {
+    if (this.compRef) {
+      this.compRef.destroy();
+    }
+  }
+
 
   public closeModal(): any {
-    return _ => {
+    return () => {
       this.listener.closeModal();
     };
   }
+}
+
+export interface ComponentPropertyModel {
+  key: string;
+  value: any;
 }
