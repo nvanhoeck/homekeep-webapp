3e57cdc8b7235ec9486457ffe3f10c06eb0642ce
Niko VH <niko.vanhoeck@gmail.com>
2020-01-25 18:01:53 +0100

    [HOME]: dbs configuration for heroku

--- src/app/core/storage/ngx-indexed-db/ngx-indexed-db.module.ts
+++ src/app/core/storage/ngx-indexed-db/ngx-indexed-db.module.ts
@@ -1,32 +1,75 @@
-import {NgModule, Type} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {DBConfig, NgxIndexedDBModule, ObjectStoreSchema} from 'ngx-indexed-db';
-import {RoomItemModel, RoomModel} from '../../../shared/models';
+import {DBConfig, NgxIndexedDBModule} from 'ngx-indexed-db';
 
-export function buildModel(type: Type<any>): ObjectStoreSchema[] {
-  return Object.keys(type)
-    .map(key => {
-      return {
-        name: key,
-        keypath: key,
-        options: {unique: false}
-      } as ObjectStoreSchema;
-    });
+
+// Ahead of time compiles requires an exported function for factories
+export function migrationFactoryExec() {
+  // The animal table was added with version 2 but none of the existing tables or data needed
+  // to be modified so a migrator for that version is not included.
+  return [];
 }
 
 const dbConfig: DBConfig = {
-  name: 'homekeepDB',
+  name: 'homekeepDatabase',
   version: 1,
   objectStoresMeta: [{
     store: 'rooms',
     storeConfig: {keyPath: 'id', autoIncrement: true},
-    storeSchema: buildModel(RoomModel),
-  },
-    {
-      store: 'items',
-      storeConfig: {keyPath: 'id', autoIncrement: true},
-      storeSchema: buildModel(RoomItemModel),
-    }],
+    storeSchema: [
+      {
+        name: 'name',
+        keypath: 'name',
+        options: {unique: false}
+      },
+      {
+        name: 'icon',
+        keypath: 'icon',
+        options: {unique: false}
+      }
+    ]
+  }, {
+    store: 'items',
+    storeConfig: {keyPath: 'id', autoIncrement: true},
+    storeSchema: [
+      {
+        name: 'name',
+        keypath: 'name',
+        options: {unique: false}
+      },
+      {
+        name: 'costPerItem',
+        keypath: 'costPerItem',
+        options: {unique: false}
+      },
+      {
+        name: 'totalCost',
+        keypath: 'totalCost',
+        options: {unique: false}
+      },
+      {
+        name: 'spendedCost',
+        keypath: 'spendedCost',
+        options: {unique: false}
+      },
+      {
+        name: 'amountWanted',
+        keypath: 'amountWanted',
+        options: {unique: false}
+      },
+      {
+        name: 'amountOwned',
+        keypath: 'amountOwned',
+        options: {unique: false}
+      },
+      {
+        name: 'roomId',
+        keypath: 'roomId',
+        options: {unique: false}
+      },
+    ]
+  }],
+  migrationFactory: migrationFactoryExec
 };
 
 
