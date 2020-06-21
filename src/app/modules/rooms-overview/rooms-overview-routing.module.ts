f3c79a49faac7e85cdc8c7814a4be41d492f6e69
Niko VH <niko.vanhoeck@gmail.com>
2020-06-21 13:13:06 +0200

    [HOME]: Shutdown guared

--- src/app/modules/rooms-overview/rooms-overview-routing.module.ts
+++ src/app/modules/rooms-overview/rooms-overview-routing.module.ts
@@ -1,14 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {RoomsOverviewComponent} from './rooms-overview-page/rooms-overview.component';
-import {AppAuthGuard} from '../../core/guards/app-auth.guard';
 
 
 const routes: Routes = [
   {
     path: 'rooms',
     component: RoomsOverviewComponent,
-    canActivate: [AppAuthGuard],
+    // canActivate: [AppAuthGuard],
     children: []
   }
 ];
