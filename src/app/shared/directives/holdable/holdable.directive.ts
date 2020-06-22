31a5c6a83013abf9022468243ef5b1555af75b94
Niko VH <niko.vanhoeck@gmail.com>
2020-06-22 17:37:32 +0200

    !fixup: ignore linting command

--- src/app/shared/directives/holdable/holdable.directive.ts
+++ src/app/shared/directives/holdable/holdable.directive.ts
@@ -2,7 +2,9 @@ import {Directive, EventEmitter, HostListener, Output} from '@angular/core';
 import {interval, Observable, Subject} from 'rxjs';
 import {filter, takeUntil, tap} from 'rxjs/operators';
 
+
 @Directive({
+  // tslint:disable-next-line:directive-selector
   selector: '[app-holdable]'
 })
 export class HoldableDirective {
