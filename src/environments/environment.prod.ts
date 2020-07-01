f2464c2f25e8a1e293d149528e1c1dc13a6e89c0
Niko VH <niko.vanhoeck@gmail.com>
2020-07-01 19:56:34 +0200

    !fixup: Backend suffixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@
 export const environment = {
   production: true,
   loggingLevel: 0,
-  roomsService: 'https://homekeep-apim.azure-api.net/'
+  roomsService: 'https://homekeep-apim.azure-api.net/homekeep-rooms',
+  roomsItemsService: 'https://homekeep-apim.azure-api.net/homekeep-rooms-items'
 };
