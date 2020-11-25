dd6e9676b58cc471d47b693e52933862d6db4c08
Niko VH <niko.vanhoeck@gmail.com>
2020-11-25 20:41:35 +0100

    [HOME]: google analytics integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+// tslint:disable-next-line:ban-types
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-XQ43KMH87P', {
+          page_path: event.urlAfterRedirects
+        });
+      }
+    });
+  }
 }
